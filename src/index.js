730e1bc5f81b89eed3d63e27c882c3e3683dd90f
Nastia Mazur <anastasiamazur2006@gmail.com>
2026-03-30 16:31:57 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@
 
 const { createServer } = require('./createServer');
 
-createServer().listen(5700, () => {
-  console.log('Server is running on localhost:5700');
-});
-
 const { sequelize } = require('./db');
 
 const app = createServer();
